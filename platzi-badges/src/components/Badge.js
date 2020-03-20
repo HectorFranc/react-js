a765c79bfa4fc343c36ffbaaaed7b339023540b2
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-20 14:08:58 -0600

    Added styles. Added bootstrap.

--- platzi-badges/src/components/Badge.js
+++ platzi-badges/src/components/Badge.js
@@ -1,25 +1,30 @@
 import React from 'react'
 
 import confLogo from '../images/badge-header.svg'
+import './styles/Badge.css';
 
 class Badge extends React.Component {
   render() {
-    return <div>
-      <div>
+    return <div className="Badge">
+      <div className="Badge__header">
         <img src={confLogo} alt="Logo de la conferencia" />
       </div>
 
-      <div>
-        <img src="https://www.gravatar.com/avatar?d=identicon" alt="Avatar" />
+      <div className="Badge__section-name">
+        <img
+          className="Badge__avatar"
+          src="https://www.gravatar.com/avatar?d=identicon"
+          alt="Avatar"
+        />
         <h1>My <br/> name</h1>
       </div>
 
-      <div>
-        <p>Developer</p>
-        <p>@sparragus</p>
+      <div className="Badge__section-info">
+        <h3>Developer</h3>
+        <div>@sparragus</div>
       </div>
 
-      <div>#platziConf</div>
+      <div className="Badge__footer">#platziConf</div>
     </div>
   }
 }
