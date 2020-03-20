2a264f6143f63d015675a01643a53cb6b822c223
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-20 13:23:36 -0600

    2.-JSX (azúcar sintética para React)

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,7 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-const element = <h1>Hello, Platzi Badges</h1>
+// const element = <h1>Hello, Platzi Badges</h1>
+// const element = React.createElement('h1', {}, 'Hello, Platzi Badges')
+const aFunction = () => 'world'
+const element = <h1>Hello, {aFunction()}</h1>
+
 const container = document.getElementById('app')
 
+// ReactDOM.render(__que__, __donde__)
 ReactDOM.render(element, container)
