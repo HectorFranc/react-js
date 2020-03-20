56e9ba409e9c73e4905592052fc919940eba65e0
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-20 14:57:43 -0600

    BadgeNew page. Components inside components

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.css'
 
 import './global.css'
-import Badge from './components/Badge'
+import BadgeNew from './pages/BadgeNew'
 
 // const element = <h1>Hello, Platzi Badges</h1>
 // const element = React.createElement('h1', {}, 'Hello, Platzi Badges')
@@ -11,11 +11,4 @@ import Badge from './components/Badge'
 const container = document.getElementById('app')
 
 // ReactDOM.render(__que__, __donde__)
-ReactDOM.render(
-  <Badge
-    firstName="Hector"
-    lastName="Torres"
-    avatarUrl="https://www.gravatar.com/avatar?d=identicon"
-    jobTitle="Developer"
-    twitter="HectorFranc0"
-  />, container)
+ReactDOM.render(<BadgeNew />, container)
---
+++ platzi-badges/src/pages/BadgeNew.js
@@ -0,0 +1,36 @@
+import React from 'react'
+
+import './styles/BadgeNew.css'
+import header from '../images/badge-header.svg'
+import Navbar from '../components/Navbar'
+import Badge from '../components/Badge'
+
+class BadgeNew extends React.Component {
+  render() {
+    return (
+      <div>
+        <Navbar />
+
+        <div className="BadgeNew__hero">
+          <img className="img-fluid" src={header} alt="Logo"/>
+        </div>
+
+        <div className="container">
+          <div className="row">
+            <div className="col">
+              <Badge
+                firstName="Hector"
+                lastName="Torres"
+                avatarUrl="https://www.gravatar.com/avatar?d=identicon"
+                jobTitle="Developer"
+                twitter="HectorFranc0"
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default BadgeNew
