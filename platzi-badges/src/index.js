d463fab5a5095d8de3b582d632f870cea07ae552
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-20 13:13:52 -0600

    1.-ReactDOM.render

--- platzi-badges/src/index.js
+++ platzi-badges/src/index.js
@@ -1,7 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-const container = document.getElementById('app');
+const element = <h1>Hello, Platzi Badges</h1>
+const container = document.getElementById('app')
 
-const element = <h1>Hello, Platzi Badges</h1>;
-ReactDOM.render(element, container);
+ReactDOM.render(element, container)
