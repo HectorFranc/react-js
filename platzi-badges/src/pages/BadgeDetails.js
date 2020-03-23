fbb8ed03b2b573ee07df6feaf24b6d22c1b19f69
Hector Franco Torres <tor6.franco6@hotmail.com>
2020-03-23 12:11:54 -0600

    Hooks: useState and custom hooks

--- platzi-badges/src/pages/BadgeDetails.js
+++ platzi-badges/src/pages/BadgeDetails.js
@@ -7,7 +7,19 @@ import "./styles/BadgeDetails.css"
 import confLogo from '../images/platziconf-logo.svg'
 import DeleteBadgeModal from '../components/DeleteBadgeModal'
 
+function useIncreaseCount(max) {
+  const [ count, setCount ] = React.useState(0)
+  if (count > max) {
+    setCount(0)
+  }
+
+  return [ count, setCount ]
+}
+
 function BadgeDetails(props) {
+  const badge = props.badge
+  const [ count, setCount ] = useIncreaseCount(4)
+
   return (
     <div>
       <div className="BadgeDetails__hero">
@@ -17,7 +29,7 @@ function BadgeDetails(props) {
               <img src={confLogo} alt="Logo de la conferencia" />
             </div>
             <div className="col-6 BadgeDetails__hero-attendant-name">
-              <h1>{props.badge.firstName} {props.badge.lastName}</h1>
+              <h1>{badge.firstName} {badge.lastName}</h1>
             </div>
           </div>
         </div>
@@ -27,18 +39,21 @@ function BadgeDetails(props) {
         <div className="row">
           <div className="col">
             <Badge
-              firstName={props.badge.firstName}
-              lastName={props.badge.lastName}
-              email={props.badge.email}
-              twitter={props.badge.twitter}
-              jobTitle={props.badge.jobTitle}
+              firstName={badge.firstName}
+              lastName={badge.lastName}
+              email={badge.email}
+              twitter={badge.twitter}
+              jobTitle={badge.jobTitle}
             />
           </div>
           <div className="col">
             <h2>Action</h2>
             <div>
               <div>
-                <Link to={`/badges/${props.badge.id}/edit`} className="btn btn-primary mb-4">Edit</Link>
+                <button onClick={() => {
+                  setCount(count + 1)
+                }} className="btn btn-primary mr-4">Increase Count {count}</button>
+                <Link to={`/badges/${badge.id}/edit`} className="btn btn-primary mb-4">Edit</Link>
               </div>
               <div>
                 <button onClick={props.onOpenModal} className="btn btn-danger">Delete</button>
